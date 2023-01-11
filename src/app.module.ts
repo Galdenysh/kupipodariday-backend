3a2361408fa3d0048c0629e9b8a81bd3dc8adbf7
Igor Petrov <galdenysh@MacBook-Air-Igor.local>
2023-01-11 05:19:09 +0300

    feat: add db and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import registerAs from './config/db.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [registerAs],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
