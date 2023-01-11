62834aec6430bae55040f51ac2066a9d2caac86b
Igor Petrov <galdenysh@MacBook-Air-Igor.local>
2023-01-11 15:13:54 +0300

    feat: add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,58 @@
-export class User {}
+import { IsEmail, IsNotEmpty, IsString, IsUrl, Length } from 'class-validator';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Wish } from 'src/wishes/entities/wish.entity';
+import { Offer } from 'src/offers/entities/offer.entity';
+import { Wishlist } from 'src/wishlists/entities/wishlist.entity';
+
+@Entity()
+export class User {
+  // id
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // createdAt
+  @Column()
+  createdAt: Date;
+
+  // updatedAt
+  @Column()
+  updatedAt: Date;
+
+  // username
+  @Column()
+  @IsString()
+  @Length(2, 30)
+  @IsNotEmpty()
+  username: string;
+
+  // about
+  @Column({ default: 'Пока ничего не рассказал о себе' })
+  @IsString()
+  @Length(2, 200)
+  about: string;
+
+  // avatar
+  @Column({ default: 'https://i.pravatar.cc/300' })
+  @IsUrl()
+  avatar: string;
+
+  // email
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  // password
+  @Column()
+  @IsString()
+  password: string;
+
+  // relationships
+  @OneToMany(() => Wish, (wish) => wish)
+  wishes: Wish[];
+
+  @OneToMany(() => Offer, (offer) => offer)
+  offers: Offer[];
+
+  @OneToMany(() => Wishlist, (wishlist) => wishlist)
+  wishlists: Wishlist[];
+}
